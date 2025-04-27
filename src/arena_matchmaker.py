67516b1e66ff33bcc668e270483874d8776f87b3
Maksim Rud <makru150@mail.ru>
2025-04-27 23:58:40 +0300

    Создание бота в матчмейкинге

--- src/arena_matchmaker.py
+++ src/arena_matchmaker.py
@@ -1,12 +1,16 @@
-#from telegram import Bot
-#import os
+from telegram import Bot
+import os
 from src.infrastructure.database import SessionLocal
 from src.application.arena_service import ArenaService
 # import time
 import asyncio
 
-async def run_arena_matchmaking(bot, interval: int = 5):
+# bot = Bot(token = os.getenv('BOT_TOKEN'))
+
+async def run_arena_matchmaking(interval: int = 5):
+    bot = Bot(token = os.getenv('BOT_TOKEN'))
     print("ARENA MATCHMAKING START")
+
     while True:
         db = SessionLocal()
         try:
